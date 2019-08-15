bc328bb1346acf447956383edb5c4cd0e2add9e8
Ubuntu <ubuntu@ip-172-31-81-66.ec2.internal>
2019-08-15 11:03:57 +0000

    change of small details

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -63,19 +63,20 @@
              $("#gh-user-data").html(userInformationHTML(userData));
              $("#gh-repo-data").html(repoInformationHTML(repoData));
          },
-         function(errorResponse) {
-             if (errorResponse.status === 404) {
-                 $("#gh-user-data").html(
-                     `<h2>No info found for user ${username}</h2>`);
-             } else if(errorResponse.status === 403) {
-                 var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
-                 $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+         
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
-                 console.log(errorResponse);
-                 $("#gh-user-data").html(
-                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-             }
-         });
- }
- 
- $(document).ready(fetchGitHubInformation);
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+}
+
+$(document).ready(fetchGitHubInformation);
