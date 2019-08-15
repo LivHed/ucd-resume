06416790412385d1765e438864703cf345ae2a82
Ubuntu <ubuntu@ip-172-31-81-66.ec2.internal>
2019-08-15 09:58:14 +0000

    remove bug to clear repo data when needed

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -38,6 +38,8 @@
  }
 
  function fetchGitHubInformation(event) {
+     $("#gh-user-data").html("");
+     $("#gh-repo-data").html("");
 
      var username = $("#gh-username").val();
      if (!username) {
